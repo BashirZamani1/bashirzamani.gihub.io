fd731c6bc58d9de3c9c5dbc57eedde260b942525
BashirZamani1 <bashirzamanidev@gmail.com>
2023-11-01 17:24:45 +0430

    Initial Commit

---
+++ js/init.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022 Marketify
+ * Author: Marketify
+ * This file is made for CURRENT TEMPLATE
+*/
+
+jQuery(document).ready(function(){
+
+	"use strict";
+	
+	// here all ready functions
+
+	tokyo_tm_cursor();
+});
+
+// -----------------------------------------------------
+// ------------------   CURSOR    ----------------------
+// -----------------------------------------------------
+
+function tokyo_tm_cursor(){
+    "use strict";
+	
+	var myCursor = jQuery('.mouse-cursor');
+	
+	if(myCursor.length){
+		if ($("body")) {
+        const e = document.querySelector(".cursor-inner"),
+            t = document.querySelector(".cursor-outer");
+        let n, i = 0,
+            o = !1;
+        window.onmousemove = function (s) {
+            o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
+        }, $("body").on("mouseenter", "a, .cursor-pointer", function () {
+            e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
+        }), $("body").on("mouseleave", "a, .cursor-pointer", function () {
+            $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
+        }), e.style.visibility = "visible", t.style.visibility = "visible"
+    }
+	}
+};
